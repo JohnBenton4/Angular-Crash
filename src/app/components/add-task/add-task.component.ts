5f529916f4c464199d32104d9a62e9b97e2cd3d7
John Benton <jtbentoniv@gmail.com>
2023-08-30 21:36:42 -0400

    button color change

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,23 +1,31 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-import {Task} from '../../Task';
+import { UiService } from '../../services/ui.service';
+import { Subscription } from 'rxjs';
+import { Task } from '../../Task';
 
 @Component({
   selector: 'app-add-task',
   templateUrl: './add-task.component.html',
-  styleUrls: ['./add-task.component.css']
+  styleUrls: ['./add-task.component.css'],
 })
-export class AddTaskComponent implements OnInit{
+export class AddTaskComponent implements OnInit {
   @Output() onAddTask: EventEmitter<Task> = new EventEmitter();
-text: string;
-day: string;
-reminder: boolean = false;
+  text: string;
+  day: string;
+  reminder: boolean = false;
+  showAddTask: boolean;
+  subscription: Subscription;
 
-  constructor() {}
+  constructor(private uiService: UiService) {
+    this.subscription = this.uiService
+      .onToggle()
+      .subscribe((value) => (this.showAddTask = value));
+  }
 
-  ngOnInit(): void{}
+  ngOnInit(): void {}
 
   onSubmit() {
-    if(!this.text) {
+    if (!this.text) {
       alert('Please add a task!');
       return;
     }
@@ -25,7 +33,7 @@ reminder: boolean = false;
     const newTask = {
       text: this.text,
       day: this.day,
-      reminder: this.reminder
+      reminder: this.reminder,
     };
 
     this.onAddTask.emit(newTask);
@@ -33,6 +41,5 @@ reminder: boolean = false;
     this.text = '';
     this.day = '';
     this.reminder = false;
-
   }
 }
